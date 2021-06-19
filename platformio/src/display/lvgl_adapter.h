f5610b3978d2dd2917749ebaae3da3c952a7b5c0
Zapta <zapta@users.noreply.github.com>
2021-06-18 22:39:06 -0700

    Minor cleanup. Eliminated 'extern' in two files.

--- platformio/src/display/lvgl_adapter.h
+++ platformio/src/display/lvgl_adapter.h
@@ -5,14 +5,13 @@
 
 namespace lvgl_adapter {
 
-extern void setup();
+void setup();
 // Called once after the first LVGL screen was rendered.
-extern void backlight_on();
+void backlight_on();
 
 // For developement.
-extern void dump_stats();
-extern void start_screen_capture();
-extern void stop_screen_capture();
-
+void dump_stats();
+void start_screen_capture();
+void stop_screen_capture();
 
 }  // namespace lvgl_adapter
--- platformio/src/display/touch_driver.h
+++ platformio/src/display/touch_driver.h
@@ -4,8 +4,9 @@
 #include <stdint.h>
 
 namespace touch_driver {
-extern void setup();
-extern void test();
-extern void touch_read(uint16_t* x, uint16_t* y, bool* is_pressed);
+    
+void setup();
+void test();
+void touch_read(uint16_t* x, uint16_t* y, bool* is_pressed);
 
 }  // namespace touch_driver
